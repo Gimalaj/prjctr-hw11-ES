867105d442889402d5b49bc357d437ffc1b8707c
Andriyan Koval <Gimalaj@gmail.com>
2023-03-29 20:19:15 +0300

    final changes

--- nginx/html/index.php
+++ nginx/html/index.php
@@ -13,6 +13,34 @@ if ($client->ping()) {
 } else {
     echo "Failed to connect to Elasticsearch.";
 }
-
-phpinfo();
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Test</title>
+
+    <script>
+        $( function() {
+            $( "#text" ).autocomplete({
+                source: "search.php",
+                minLength: 2,
+
+            });
+        } );
+    </script>
+</head>
+<body>
+
+<div class="ui-widget">
+    <label for="text">Text: </label>
+    <input id="text">
+</div>
+
+
+
+
+</body>
+</html>
